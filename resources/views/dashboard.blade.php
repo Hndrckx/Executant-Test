bc5fd3e039e7cea379392d7162e04dd783c5984a
Hndrckx <90184045+Hndrckx@users.noreply.github.com>
2022-05-06 10:37:03 +0200

    Routes

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,11 +5,21 @@
         </h2>
     </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
-                    You're logged in!
+    <div class="flex">
+        <div class="flex flex-col rounded-md bg-white mt-3 w-52">
+            <a class="mx-3 text-center my-5 rounded-lg border-2 w-28" href="/avatars">Avatars</a>
+            <a class="mx-3 text-center my-5 rounded-lg border-2 w-28" href="/images">Images</a>
+            <a class="mx-3 text-center my-5 rounded-lg border-2 w-28" href="/categories">Catégories</a>
+            <a class="mx-3 text-center my-5 rounded-lg border-2 w-28" href="">Galerie</a>
+            <a class="mx-3 text-center my-5 rounded-lg border-2 w-28" href="/users">Urilisateurs</a>
+        </div>
+    
+        <div class="py-12 min-w-full">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                    <div class="p-6 bg-white border-b border-gray-200">
+                        You're logged in!
+                    </div>
                 </div>
             </div>
         </div>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,12 @@
 <?php
 
+use App\Http\Controllers\AvatarController;
+use App\Http\Controllers\CategorieController;
+use App\Http\Controllers\ImageController;
+use App\Http\Controllers\UserController;
+use App\Models\Avatar;
+use App\Models\Categorie;
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,5 +27,44 @@ Route::get('/', function () {
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
+
+Route::get('/avatars', function () {
+    $allAvatars = Avatar::all();
+    return view('layouts.avatars', compact('allAvatars'));
+});
+
+Route::get('/createAvatars', function () {
+    $avatars = Avatar::all();
+    return view('layouts.createAvatar', compact('avatars'));
+});
+
+Route::post('addava', [AvatarController::class, 'store']);
+
+Route::delete('delav/{id}', [AvatarController::class, 'delete']);
+
+Route::get('/users', function () {
+    $users = User::all();
+    return view('layouts.users', compact('users'));
+});
+
+Route::get('/images', function () {
+    return view('layouts.images');
+});
+
+Route::post('addima', [ImageController::class, 'store']);
+
+Route::get('/editUser/{id}/editUser', [UserController::class, 'edit']);
+Route::put('/update/{id}/update', [UserController::class, 'update']);
+
+
+Route::get('/categories', function () {
+    $allCategorie = Categorie::all();
+    return view('layouts.categories', compact('allCategorie'));
+});
+
+Route::get('createCategorie', [CategorieController::class, 'create']);
+Route::post('addcat', [CategorieController::class, 'store']);
+Route::get('/editCategorie/{id}/editCategorie', [CategorieController::class, 'edit']);
+Route::put('/updateCategorie/{id}/updateCategorie', [CategorieController::class, 'update']);
 
 require __DIR__.'/auth.php';
